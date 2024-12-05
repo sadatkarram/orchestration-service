4ac4711d921ead9de464387985835d9106b861eb
Ram Sadatkar <ramsadatkar@fynd.team>
2024-12-06 00:47:34 +0530

    project setup

---
+++ src/main/java/com/digital/api/orchestration_service/OrchestrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digital.api.orchestration_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrchestrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrchestrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/api/orchestration_service/OrchestrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.api.orchestration_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrchestrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
